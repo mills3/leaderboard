5ea129c015fb2fa33a2a3d035cc95ee9fcda8b1c
alan moir <mills3@gmx,co.uk>
2017-05-16 21:30:43 +0100

    added url for Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,7 @@ import './Contact.css';
 const Contact = (props) => {
   return (
     <div className="contact">
-      <a href="#" target="_blank">Coded by <span>Mills3</span></a>
+      <a href="https://github.com/mills3/leaderboard" target="_blank">Coded by <span>Mills3</span></a>
     </div>
   );
 }
